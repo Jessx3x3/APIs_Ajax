88c9629c6ed6c385d1e57e84a4acbc705fd2f5cb
Richard <richard.eneene@gmail.com>
2020-02-18 22:47:54 -0300

    datos listos

--- Pokedex/javascript.js
+++ Pokedex/javascript.js
@@ -1,75 +1,73 @@
 function load(onOff){
+
     onOff ? $("#load").show() : $("#load").hide();
-}
-
-function displayPokemon(){
-    $('#content').hide();
-    $('#pokemon_info').hide();
-
-    var pokemon = '';
-
-    for (var i = 1; i < 200; i++) {
-
-        pokemon += "<div id='pokemon'><img id='" + i + "' src='http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + i + ".png'></div>";
-       
-    }
-
-    $('#content').append(pokemon);
-
-    setTimeout(function(){
-        $('#content').show();
-        load(false);
-    }, 5000);
 
 }
 
-var img_pokemon = [];
+        function displayPokemon(){    
 
-function img_data(url, id){
-    var img_pokemon = url+id+".png";
-    console.log(img_pokemon);
-    $(".img_pokemon").attr("src", img_pokemon);
-}
+            var image = '';
 
+            var url_sprites = '';
 
-$(document).ready( function(){
+                for (let i = 1; i < 200; i++) {
+                    url_sprites = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + i + '.png'
+                image = '<img class="pokemon" id="' + i + '" src="' + url_sprites + '">';
 
-    var name_pokemon = [];
-    var url = "http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/";
+                $('#content').append(image);
 
-    load(true);
-    displayPokemon();
+                }
+                setTimeout(function(){
+                    $('#content').show();
+                    load(false);
+                }, 5000);
+        }   
 
-    $("img").click(function(){
-        var id_pulsado = $(this).attr('id');
-        console.log(id_pulsado);
-        
-        if(id_pulsado){
-            var nombre = name_pokemon[id_pulsado];
-            console.log(nombre);
+        $(document).ready(function () {
+
+            $("#pokemon_info").hide();
+
+            load(true);
+
+            displayPokemon();
+        });
+
+        $(document).on("click", ".pokemon", function () {
+
+            var id = $(this).attr('id');
+
             $("#pokemon_info").show();
-            $(".name_d").text(nombre);
-            img_data(url, id_pulsado);
-        }
 
-    })
+            $(".img_pokemon").attr("src", 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + id + '.png');
+            
+            $("button").click(function(){
 
-    $("button").click(function(){
-        $("#pokemon_info").hide();
-    })
+                $("#pokemon_info").hide();
 
-    $.ajax({
-        type: "GET",
-        url: "https://pokeapi.co/api/v2/pokemon?limit=200",
-        success: function(data) {
-            for(var i=1; i<200; i++){
-                name_pokemon[i] = data.results[i].name;
-                console.log(name_pokemon[i]);
-            }   
-        },
-        error: function (xhr, status) {  
-            if(xhr.status === 404)
-            console.log("Error, pokemon not found");
-        }    
-    });
-});
+            });
+
+            var datos = {};
+
+            var url = "http://pokeapi.co/api/v2/pokemon/";
+
+            $.get(url + id + "/", function (datos) {
+
+                $(".name_d").html(datos.name);
+
+                $(".tipo_1").html(datos.types[0].type.name);
+
+                if (datos.types.length == 2) {
+
+                    $(".tipo_2").html(datos.types[1].type.name);
+
+                    $(".tipo_2").show();
+
+                }else $(".tipo_2").hide();
+
+                $(".weight").html(datos.weight);
+
+                $(".height").html(datos.height);
+
+            }, 'json');
+
+        });
