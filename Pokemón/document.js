80d985b5e28a9f33eacd3d8f0d6a16e7d601fc00
Jessx3x3 <j.vasquez12@ufromail.cl>
2020-02-21 12:18:59 -0300

    cambio

--- Pokemón/document.js
+++ Pokemón/document.js
@@ -1,9 +1,12 @@
 function displayPokemon(){
     var pokemon = '';
         for (var i = 1; i < 151; i++) {
+
             pokemon="<div class='pokemon'><img src='http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+i+".png'></div>";
+            
             $('#box').append(pokemon);
         }  
+        
 }
 $(document).ready(function(){
     displayPokemon();
