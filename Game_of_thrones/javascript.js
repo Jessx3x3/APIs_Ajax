f2d6b9e5f4c9e78829bbb25f96a1c97206c348c2
Jessx3x3 <j.vasquez12@ufromail.cl>
2020-02-20 23:43:42 -0300

    mod. got

--- Game_of_thrones/javascript.js
+++ Game_of_thrones/javascript.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
+
+    var starkID = 362; var targaryenID = 378; var lannisterID = 229; var baratheonID = 15;
+
+    $('#information').hide();
+    $('h1').hide();
+
+    $('#362').click(function(){
+        getValues(starkID);
+    })
+    $('#378').click(function(){
+        getValues(targaryenID);
+    })
+    $('#229').click(function(){
+        getValues(lannisterID);
+    })
+    $('#15').click(function(){
+        getValues(baratheonID);
+    })
+
+    function getValues(id){
+
+            var url_s = 'https://anapioficeandfire.com/api/houses/'+id;
+
+            $.get(url_s, function(res) {
+                var name = res.name; $('.nombre').text(name);
+                var frase = res.words; $('.frases').text(frase);
+                var title = res.titles; $('.titulos').text(title);
+                $('h1').show();$('#information').show();
+
+            }, 'json');
+
+            return false;
+    }
     
 });
